06a05e61c1d6725763dd7d64db5ce93ce81fd74e
user <user@users-MacBook-Pro.local>
2022-08-06 18:59:36 +0900

    feat: reset css and add a simple face

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<slot />
+
+<style>
+	:global(html, body, h1, h2, h3, h4, p, span, button) {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		box-sizing: border-box;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bb-bob">
+	<div class="face">
+		<div class="eyes">
+			<div class="eye" />
+			<div class="eye" />
+		</div>
+		<div class="mouth" />
+	</div>
+</div>
+
+<style lang="scss">
+	.bb-bob {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		width: 100vw;
+		background-color: #f0f0f0;
+	}
+	.face {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	.eyes {
+		display: flex;
+		justify-content: center;
+		gap: 60px;
+	}
+	.eye {
+		width: 30px;
+		height: 50px;
+		background: #222222;
+	}
+	.mouth {
+		width: 30px;
+		height: 50px;
+		background: #222222;
+	}
+</style>
