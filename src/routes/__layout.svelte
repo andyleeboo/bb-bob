35661f282f122c52b05c0cf28cff06463d7db99a
Byungho <andyleeboo92@gmail.com>
2022-08-06 20:31:37 +0900

    feat: add pwa settings and some other minor stuffs

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,17 @@
 	import '../styles/style.css';
 </script>
 
+<svelte:head>
+	<title>BB-Bob</title>
+</svelte:head>
+
 <slot />
+
+<style>
+	:global(html, body, h1, h2, h3, h4, p, span, button) {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		box-sizing: border-box;
+	}
+</style>
